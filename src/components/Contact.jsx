f03799f680fc9ee692c60fb57979380552554c61
yhujae <yhujayp757@gmail.com>
2023-11-28 14:54:30 -0800

    adjusted address

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,158 +1,201 @@
-import React, {useEffect} from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelopeCircleCheck, faMapLocationDot, faPhone} from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelopeCircleCheck,
+  faMapLocationDot,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
 
-
-const boxShadow = '0 0 15px rgba(0, 0, 0, 0.1)'; // Box shadow style
+const boxShadow = "0 0 15px rgba(0, 0, 0, 0.1)"; // Box shadow style
 const headingStyle = {
-  marginTop: '7rem',
-  color: '#000000',
-  fontWeight: 'bold',
-  textShadow: '-20px -20px 3px rgba(0, 0, 0, 0.1)',
+  marginTop: "7rem",
+  color: "#000000",
+  fontWeight: "bold",
+  textShadow: "-20px -20px 3px rgba(0, 0, 0, 0.1)",
 };
 
 const buttonStyle = {
-  marginTop: '1rem',
-  backgroundColor: '#cc1616',
-  fontWeight:'bold',
-  padding: '0.3rem 1rem',
-  border: 'red 2px bold',
-  borderRadius: '5px',
-  cursor: 'pointer',
-  marginRight: '1rem',
-  transition: 'background-color 0.6s ease-out'
+  marginTop: "1rem",
+  backgroundColor: "#cc1616",
+  fontWeight: "bold",
+  padding: "0.3rem 1rem",
+  border: "red 2px bold",
+  borderRadius: "5px",
+  cursor: "pointer",
+  marginRight: "1rem",
+  transition: "background-color 0.6s ease-out",
 };
-
-
 
 function Contact() {
   useEffect(() => {
     AOS.init();
   }, []);
   return (
-    <div id='contact' className="container mt-4">
-      <h2 className="aos-init aos-animate" data-aos="fade-up"
-      style={headingStyle}>CONTACT</h2>
+    <div id="contact" className="container mt-4">
+      <h2
+        className="aos-init aos-animate"
+        data-aos="fade-up"
+        style={headingStyle}
+      >
+        CONTACT
+      </h2>
       <div className="row mt-3 aos-init aos-animate" data-aos="fade-up">
+        <div className="col-md-4">
           <div
-            className="col-md-4"
+            className="p-3 mt-3"
+            style={{ height: "160px", boxShadow: boxShadow }}
           >
-            <div className="p-3 mt-3"
-              style={{ height: '160px',boxShadow: boxShadow}}
+            <FontAwesomeIcon
+              icon={faMapLocationDot}
+              style={{ fontSize: "2rem", color: "#e82d2d" }}
+            />
+            <h4
+              style={{ color: "#777777", marginTop: "5px", fontWeight: "bold" }}
             >
-            <FontAwesomeIcon icon={faMapLocationDot} style={{ fontSize: '2rem' , color:'#e82d2d'}}  />
-            <h4  style={{ color:'#777777', marginTop:'5px', fontWeight:'bold'}}>Our Address</h4>
-            <p style={{ color:'#777777'}}>A108 Adam Street, FCT, Abuja 535022</p>
-            </div>
-          </div>  
-                   
-           
-          <div className="col-md-4">
-            <div className="p-3  mt-3"
-              style={{ height: '180px', boxShadow: boxShadow}}>
-             <FontAwesomeIcon icon={faEnvelopeCircleCheck} style={{ fontSize: '2rem' , color:'#e82d2d'}}  />
-            <h4 style={{ color:'#777777', marginTop:'5px', fontWeight:'bold'}}>Email Us</h4>
-            <p style={{ color:'#777777'}}>contact@hotmail.com</p>
-            </div>
+              Our Address
+            </h4>
+            <p style={{ color: "#777777" }}>
+              B208 Aguyiam Street, FCT, Abuja 535022
+            </p>
           </div>
-          <div className="col-md-4" >
-            <div className="p-3  mt-3"
-              style={{ height: '160px', boxShadow: boxShadow}}>
-             <FontAwesomeIcon icon={faPhone} style={{ fontSize: '2rem' , color:'#e82d2d'}}  />
-            <h4 style={{ color:'#777777', marginTop:'5px', fontWeight:'bold'}}>Call Us</h4>
-            <p style={{ color:'#777777'}}>+23481000003345</p>
-            </div>
+        </div>
+
+        <div className="col-md-4">
+          <div
+            className="p-3  mt-3"
+            style={{ height: "180px", boxShadow: boxShadow }}
+          >
+            <FontAwesomeIcon
+              icon={faEnvelopeCircleCheck}
+              style={{ fontSize: "2rem", color: "#e82d2d" }}
+            />
+            <h4
+              style={{ color: "#777777", marginTop: "5px", fontWeight: "bold" }}
+            >
+              Email Us
+            </h4>
+            <p style={{ color: "#777777" }}>contact@hotmail.com</p>
           </div>
+        </div>
+        <div className="col-md-4">
+          <div
+            className="p-3  mt-3"
+            style={{ height: "160px", boxShadow: boxShadow }}
+          >
+            <FontAwesomeIcon
+              icon={faPhone}
+              style={{ fontSize: "2rem", color: "#e82d2d" }}
+            />
+            <h4
+              style={{ color: "#777777", marginTop: "5px", fontWeight: "bold" }}
+            >
+              Call Us
+            </h4>
+            <p style={{ color: "#777777" }}>+23481000003345</p>
+          </div>
+        </div>
       </div>
 
-     <div className="row mt-4">
+      <div className="row mt-4">
         <div className="col-lg-6  aos-init aos-animate" data-aos="fade-right">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969252.421810581!2d4.389279175535705!3d9.082304270558407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1043ff5ea6ac96eb%3A0x483d2186ba9e7ff2!2sNigeria!5e0!3m2!1sen!2sus!4v1605176805585!5m2!1sen!2sus" 
-          style={{width:"30rem", height:"400px" ,frameborder:"0" ,border:'0', allowfullscreen:"" , ariaHidden:"false" ,tabIndex:"0"}}></iframe>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969252.421810581!2d4.389279175535705!3d9.082304270558407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1043ff5ea6ac96eb%3A0x483d2186ba9e7ff2!2sNigeria!5e0!3m2!1sen!2sus!4v1605176805585!5m2!1sen!2sus"
+            style={{
+              width: "30rem",
+              height: "400px",
+              frameborder: "0",
+              border: "0",
+              allowfullscreen: "",
+              ariaHidden: "false",
+              tabIndex: "0",
+            }}
+          ></iframe>
+        </div>
 
-         </div>
-
-          <div className="col-lg-6 p-4  aos-init aos-animate" data-aos="fade-left"  style={{ boxShadow: boxShadow}}>
-            <form
-              action="forms/contact.php"
-              method="post"
-              role="form"
-              className="php-email-form"
-            >
-              <div className="row">
-                <div className="col-md-6 form-group">
-                  <input
-                    type="text"
-                    name="name"
-                    className="form-control"
-                    id="name"
-                    placeholder="Your Name"
-                    required
-                  />
-                </div>
-                <div className="col-md-6 form-group mt-3 mt-md-0">
-                  <input
-                    type="email"
-                    className="form-control"
-                    name="email"
-                    id="email"
-                    placeholder="Your Email"
-                    required
-                  />
-                </div>
-              </div>
-              <div className="form-group mt-3">
+        <div
+          className="col-lg-6 p-4  aos-init aos-animate"
+          data-aos="fade-left"
+          style={{ boxShadow: boxShadow }}
+        >
+          <form
+            action="forms/contact.php"
+            method="post"
+            role="form"
+            className="php-email-form"
+          >
+            <div className="row">
+              <div className="col-md-6 form-group">
                 <input
                   type="text"
+                  name="name"
                   className="form-control"
-                  name="subject"
-                  id="subject"
-                  placeholder="Subject"
+                  id="name"
+                  placeholder="Your Name"
                   required
                 />
               </div>
-              <div className="form-group mt-3">
-                <textarea
+              <div className="col-md-6 form-group mt-3 mt-md-0">
+                <input
+                  type="email"
                   className="form-control"
-                  name="message"
-                  rows="5"
-                  placeholder="Message"
+                  name="email"
+                  id="email"
+                  placeholder="Your Email"
                   required
-                ></textarea>
+                />
               </div>
-              {/* <div className="my-3">
+            </div>
+            <div className="form-group mt-3">
+              <input
+                type="text"
+                className="form-control"
+                name="subject"
+                id="subject"
+                placeholder="Subject"
+                required
+              />
+            </div>
+            <div className="form-group mt-3">
+              <textarea
+                className="form-control"
+                name="message"
+                rows="5"
+                placeholder="Message"
+                required
+              ></textarea>
+            </div>
+            {/* <div className="my-3">
                 <div className="loading">Loading</div>
                 <div className="error-message"></div>
                 <div className="sent-message">
                   Your message has been sent. Thank you!
                 </div>
               </div> */}
-              <div className="text-center mt-4">
-                <button 
-                    className="btn btn-outline-light btn-get-started p-3 mt-2 scrollto" 
-                  style={buttonStyle} type="submit"
-                  onMouseEnter={(e) => {
-                    e.target.style.backgroundColor = 'red';
-                    e.target.style.color = 'white';
-                  }}
-                  onMouseLeave={(e) => {
-                    e.target.style.backgroundColor = '#cc1616';
-                    e.target.style.color = 'white';
-                  }}>
-                              Send Message
-                          </button>
-                        </div>
-                      </form>
-                 </div>
-              </div>
-            </div> 
-     
- 
+            <div className="text-center mt-4">
+              <button
+                className="btn btn-outline-light btn-get-started p-3 mt-2 scrollto"
+                style={buttonStyle}
+                type="submit"
+                onMouseEnter={(e) => {
+                  e.target.style.backgroundColor = "red";
+                  e.target.style.color = "white";
+                }}
+                onMouseLeave={(e) => {
+                  e.target.style.backgroundColor = "#cc1616";
+                  e.target.style.color = "white";
+                }}
+              >
+                Send Message
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default Contact;
-
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,63 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faLinkedin, faSkype, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faArrowCircleRight, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faSkype,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faArrowCircleRight,
+  faArrowUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 const boxStyle = {
-  backgroundColor: '#191919',
-  color: 'white',
-  borderTop: 'red 5px solid',
-  margin: '1.5rem',
-  padding: '20px',
+  backgroundColor: "#191919",
+  color: "white",
+  borderTop: "red 5px solid",
+  margin: "1.5rem",
+  padding: "20px",
 };
 
 const socialLinkStyle = {
-  display: 'inline-block',
-  width: '40px',
-  height: '40px',
-  borderRadius: '50%',
-  marginRight: '5px',
-  textAlign: 'center',
-  lineHeight: '40px',
-  transition: 'background-color 0.3s',
+  display: "inline-block",
+  width: "40px",
+  height: "40px",
+  borderRadius: "50%",
+  marginRight: "5px",
+  textAlign: "center",
+  lineHeight: "40px",
+  transition: "background-color 0.3s",
 };
 
 const backToTopStyle = {
-  position: 'fixed',
-  bottom: '20px',
-  right: '20px',
-  backgroundColor: '#cc1616',
-  padding: '1rem',
-  width: '50px',
-  height: '50px',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  color: 'white',
-  fontSize: '24px',
+  position: "fixed",
+  bottom: "20px",
+  right: "20px",
+  backgroundColor: "#cc1616",
+  padding: "1rem",
+  width: "50px",
+  height: "50px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "white",
+  fontSize: "24px",
 };
 
 const buttonStyle = {
-  marginTop: '-4px',
-  backgroundColor: '#cc1616',
-  fontWeight: 'bold',
-  padding: '0.8rem',
-  border: 'none',
-  borderRadius: '0',
-  cursor: 'pointer',
-  transition: 'background-color 0.6s ease-out',
+  marginTop: "-4px",
+  backgroundColor: "#cc1616",
+  fontWeight: "bold",
+  padding: "0.8rem",
+  border: "none",
+  borderRadius: "0",
+  cursor: "pointer",
+  transition: "background-color 0.6s ease-out",
 };
 
 const scrollToTop = () => {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth',
+    behavior: "smooth",
   });
 };
 
 // Define a reusable function to handle onMouseEnter and onMouseLeave events for anchor tags
 const handleAnchorHover = (e) => {
-  e.target.style.color = e.type === 'mouseenter' ? '#cc1616' : 'grey';
+  e.target.style.color = e.type === "mouseenter" ? "#cc1616" : "grey";
 };
 
 function Footer() {
@@ -72,9 +81,9 @@ function Footer() {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -85,72 +94,85 @@ function Footer() {
     setCurrentYear(new Date().getFullYear());
   }, []);
 
-
   return (
-    <div className="footer-top" style={{ backgroundColor: 'black', marginTop: '4rem' }}>
+    <div
+      className="footer-top"
+      style={{ backgroundColor: "black", marginTop: "4rem" }}
+    >
       <div className="container">
         <div className="row">
           <div className="col-lg-4 col-md-6">
             <div className="footer-info" style={boxStyle}>
               <h4>Style Sprint</h4>
               <p>
-                A108 Adam Street <br />
-                AB 535022, Nigeria<br /><br />
+                B208 Aguyiam Street,
+                <br />
+                535022 Abuja, Nigeria
+                <br />
+                <br />
                 <strong>Phone:</strong> +2340000543321 <br />
                 <strong>Email:</strong> contact@hotmail.com <br />
               </p>
               <div className="text-white social-links mt-3">
-                <div
-                  style={{ ...socialLinkStyle }}
-                  className="social-link"
-                >
+                <div style={{ ...socialLinkStyle }} className="social-link">
                   <a href="#" className="twitter">
-                    <FontAwesomeIcon icon={faTwitter} style={{ fontSize: '1.5rem', color: 'white' }} 
+                    <FontAwesomeIcon
+                      icon={faTwitter}
+                      style={{ fontSize: "1.5rem", color: "white" }}
                     />
                   </a>
                 </div>
-                <div
-                  style={{ ...socialLinkStyle }}
-                  className="social-link"
-                >
+                <div style={{ ...socialLinkStyle }} className="social-link">
                   <a href="#" className="facebook">
-                    <FontAwesomeIcon icon={faFacebook} style={{ fontSize: '1.5rem', color: 'white' }} />
+                    <FontAwesomeIcon
+                      icon={faFacebook}
+                      style={{ fontSize: "1.5rem", color: "white" }}
+                    />
                   </a>
                 </div>
-                <div
-                  style={{ ...socialLinkStyle }}
-                  className="social-link"
-                >
+                <div style={{ ...socialLinkStyle }} className="social-link">
                   <a href="#" className="instagram">
-                    <FontAwesomeIcon icon={faInstagram} style={{ fontSize: '1.5rem', color: 'white' }} />
+                    <FontAwesomeIcon
+                      icon={faInstagram}
+                      style={{ fontSize: "1.5rem", color: "white" }}
+                    />
                   </a>
                 </div>
-                <div
-                  style={{ ...socialLinkStyle }}
-                  className="social-link"
-                >
+                <div style={{ ...socialLinkStyle }} className="social-link">
                   <a href="#" className="google-plus">
-                    <FontAwesomeIcon icon={faSkype} style={{ fontSize: '1.5rem', color: 'white' }} />
+                    <FontAwesomeIcon
+                      icon={faSkype}
+                      style={{ fontSize: "1.5rem", color: "white" }}
+                    />
                   </a>
                 </div>
-                <div
-                  style={{ ...socialLinkStyle }}
-                  className="social-link"
-                >
+                <div style={{ ...socialLinkStyle }} className="social-link">
                   <a href="#" className="linkedin">
-                    <FontAwesomeIcon icon={faLinkedin} style={{ fontSize: '1.5rem', color: 'white' }} />
+                    <FontAwesomeIcon
+                      icon={faLinkedin}
+                      style={{ fontSize: "1.5rem", color: "white" }}
+                    />
                   </a>
                 </div>
               </div>
             </div>
           </div>
           <div className="text-light width-100 pt-4 col-lg-2 col-md-6 footer-links">
-            <h6 style={{ textAlign: 'left', marginLeft:'2rem' }}>Useful Links</h6>
-            <ul style={{ color: 'pink', textAlign: 'left', listStyleType: 'none', lineHeight: '2' }}>
+            <h6 style={{ textAlign: "left", marginLeft: "2rem" }}>
+              Useful Links
+            </h6>
+            <ul
+              style={{
+                color: "pink",
+                textAlign: "left",
+                listStyleType: "none",
+                lineHeight: "2",
+              }}
+            >
               <li>
-                <FontAwesomeIcon icon={faArrowCircleRight} />{' '}
+                <FontAwesomeIcon icon={faArrowCircleRight} />{" "}
                 <a
-                  style={{ color: 'gray', textDecoration: 'none' }}
+                  style={{ color: "gray", textDecoration: "none" }}
                   onMouseEnter={handleAnchorHover}
                   onMouseLeave={handleAnchorHover}
                   href="#"
@@ -159,9 +181,9 @@ function Footer() {
                 </a>
               </li>
               <li>
-                <FontAwesomeIcon icon={faArrowCircleRight} />{' '}
+                <FontAwesomeIcon icon={faArrowCircleRight} />{" "}
                 <a
-                  style={{ color: 'gray', textDecoration: 'none' }}
+                  style={{ color: "gray", textDecoration: "none" }}
                   onMouseEnter={handleAnchorHover}
                   onMouseLeave={handleAnchorHover}
                   href="#"
@@ -170,9 +192,9 @@ function Footer() {
                 </a>
               </li>
               <li>
-                <FontAwesomeIcon icon={faArrowCircleRight} />{' '}
+                <FontAwesomeIcon icon={faArrowCircleRight} />{" "}
                 <a
-                  style={{ color: 'gray', textDecoration: 'none' }}
+                  style={{ color: "gray", textDecoration: "none" }}
                   onMouseEnter={handleAnchorHover}
                   onMouseLeave={handleAnchorHover}
                   href="#"
@@ -181,9 +203,9 @@ function Footer() {
                 </a>
               </li>
               <li>
-                <FontAwesomeIcon icon={faArrowCircleRight} />{' '}
+                <FontAwesomeIcon icon={faArrowCircleRight} />{" "}
                 <a
-                  style={{ color: 'gray', textDecoration: 'none' }}
+                  style={{ color: "gray", textDecoration: "none" }}
                   onMouseEnter={handleAnchorHover}
                   onMouseLeave={handleAnchorHover}
                   href="#"
@@ -192,9 +214,9 @@ function Footer() {
                 </a>
               </li>
               <li>
-                <FontAwesomeIcon icon={faArrowCircleRight} />{' '}
+                <FontAwesomeIcon icon={faArrowCircleRight} />{" "}
                 <a
-                  style={{ color: 'gray', textDecoration: 'none' }}
+                  style={{ color: "gray", textDecoration: "none" }}
                   onMouseEnter={handleAnchorHover}
                   onMouseLeave={handleAnchorHover}
                   href="#"
@@ -205,51 +227,61 @@ function Footer() {
             </ul>
           </div>
           <div className="text-light pt-4 col-lg-2 col-md-6 footer-links">
-            <h6 style={{ textAlign: 'left', marginLeft:'2rem' }}>Our Services</h6>
-            <ul style={{ color: 'pink', textAlign: 'left', listStyleType: 'none', textDecoration: 'none', lineHeight: '2' }}>
+            <h6 style={{ textAlign: "left", marginLeft: "2rem" }}>
+              Our Services
+            </h6>
+            <ul
+              style={{
+                color: "pink",
+                textAlign: "left",
+                listStyleType: "none",
+                textDecoration: "none",
+                lineHeight: "2",
+              }}
+            >
               <li>
-                <FontAwesomeIcon icon={faArrowCircleRight} />{' '}
+                <FontAwesomeIcon icon={faArrowCircleRight} />{" "}
                 <a
                   onMouseEnter={handleAnchorHover}
                   onMouseLeave={handleAnchorHover}
-                  style={{ color: 'gray', textDecoration: 'none' }}
+                  style={{ color: "gray", textDecoration: "none" }}
                   href="#"
                 >
-                 Shopping Cart
+                  Shopping Cart
                 </a>
               </li>
               <li>
-                <FontAwesomeIcon icon={faArrowCircleRight} />{' '}
+                <FontAwesomeIcon icon={faArrowCircleRight} />{" "}
                 <a
-                  style={{ color: 'gray', textDecoration: 'none' }}
+                  style={{ color: "gray", textDecoration: "none" }}
                   onMouseEnter={handleAnchorHover}
                   onMouseLeave={handleAnchorHover}
                   href="#"
                 >
-                 User Accounts
+                  User Accounts
                 </a>
               </li>
-        
+
               <li>
-                <FontAwesomeIcon icon={faArrowCircleRight} />{' '}
+                <FontAwesomeIcon icon={faArrowCircleRight} />{" "}
                 <a
-                  style={{ color: 'gray', textDecoration: 'none' }}
+                  style={{ color: "gray", textDecoration: "none" }}
                   onMouseEnter={handleAnchorHover}
                   onMouseLeave={handleAnchorHover}
                   href="#"
                 >
-                 Online Community
+                  Online Community
                 </a>
               </li>
               <li>
-                <FontAwesomeIcon icon={faArrowCircleRight} />{' '}
+                <FontAwesomeIcon icon={faArrowCircleRight} />{" "}
                 <a
-                  style={{ color: 'gray', textDecoration: 'none' }}
+                  style={{ color: "gray", textDecoration: "none" }}
                   onMouseEnter={handleAnchorHover}
                   onMouseLeave={handleAnchorHover}
                   href="#"
                 >
-               Customer Support
+                  Customer Support
                 </a>
               </li>
             </ul>
@@ -258,18 +290,22 @@ function Footer() {
             <h6>Our Newsletter</h6>
             <p>Subscriber Exclusives</p>
             <form action="" method="post">
-              <input type="email" name="email" style={{ padding: '0.65rem', width:'300px' }} />
+              <input
+                type="email"
+                name="email"
+                style={{ padding: "0.65rem", width: "300px" }}
+              />
               <input
                 className="btn btn-outline-light btn-get-started scrollto"
                 style={buttonStyle}
                 type="submit"
                 onMouseEnter={(e) => {
-                  e.target.style.backgroundColor = 'red';
-                  e.target.style.color = 'white';
+                  e.target.style.backgroundColor = "red";
+                  e.target.style.color = "white";
                 }}
                 onMouseLeave={(e) => {
-                  e.target.style.backgroundColor = '#cc1616';
-                  e.target.style.color = 'white';
+                  e.target.style.backgroundColor = "#cc1616";
+                  e.target.style.color = "white";
                 }}
                 value="Subscribe"
               />
@@ -277,22 +313,42 @@ function Footer() {
           </div>
         </div>
       </div>
-      <hr style={{ height: '2px', width: '50rem', backgroundColor: 'gray', margin: 'auto', marginTop: '1rem' }}></hr>
+      <hr
+        style={{
+          height: "2px",
+          width: "50rem",
+          backgroundColor: "gray",
+          margin: "auto",
+          marginTop: "1rem",
+        }}
+      ></hr>
       <div className="container pt-3">
-        <div className="copyright p-2" style={{ color: '#777777' }}>
-          <p>  &copy; {currentYear} <strong><span>StyleSprint</span></strong>. All Rights Reserved</p>
+        <div className="copyright p-2" style={{ color: "#777777" }}>
+          <p>
+            {" "}
+            &copy; {currentYear}{" "}
+            <strong>
+              <span>StyleSprint</span>
+            </strong>
+            . All Rights Reserved
+          </p>
         </div>
       </div>
       {showBackToTop && (
-        <a href="#"  onClick={scrollToTop} style={backToTopStyle} className="back-to-top d-flex  align-items-center justify-content-end active"
-        onMouseEnter={(e) => {
-          e.target.style.backgroundColor = 'red';
-          e.target.style.color = 'white';
-        }}
-        onMouseLeave={(e) => {
-          e.target.style.backgroundColor = '#cc1616';
-          e.target.style.color = 'white';
-        }}>
+        <a
+          href="#"
+          onClick={scrollToTop}
+          style={backToTopStyle}
+          className="back-to-top d-flex  align-items-center justify-content-end active"
+          onMouseEnter={(e) => {
+            e.target.style.backgroundColor = "red";
+            e.target.style.color = "white";
+          }}
+          onMouseLeave={(e) => {
+            e.target.style.backgroundColor = "#cc1616";
+            e.target.style.color = "white";
+          }}
+        >
           <FontAwesomeIcon icon={faArrowUp} />
         </a>
       )}
